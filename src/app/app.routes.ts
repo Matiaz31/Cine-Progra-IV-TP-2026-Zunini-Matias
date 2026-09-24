7c61661b6ce1dcb3cc635a7951d131ead59cf34b
Matiaz31 <matiaszunini31@gmail.com>
2026-09-24 19:38:00 -0300

    Funcionamiento de los guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { Home } from './features/home/home';
+import { authGuard } from './guards/auth-guard';
+import { roleGuard } from './guards/role-guard';
 
 export const routes: Routes = [
   {
@@ -15,6 +17,25 @@ export const routes: Routes = [
   {
     path: 'home',
     component: Home,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./features/admin/admin').then(m => m.Admin),
+    canActivate: [
+      authGuard,
+      roleGuard(['admin']),
+    ],
+  },
+  {
+    path: 'empleado',
+    loadComponent: () =>
+      import('./features/empleado/empleado').then(m => m.Empleado),
+    canActivate: [
+      authGuard,
+      roleGuard(['employee']),
+    ],
   },
   {
     path: '',
